4e5af25021ce507c876a26c3ee64c67211e0e40b
jobaerahamed <robin_68@live.com>
2020-03-31 17:33:01 +0600

    More of ice king

--- assets/letsTalk.js
+++ assets/letsTalk.js
@@ -10,3 +10,16 @@ var hello = `
 `;
 
 console.log(hello, 'color: #097379', 'I like where this is going. Keep digging!')
+
+var story = `%c
+Here is a small story for you -
+
+A supposedly evil wizard capable of creating and manipulating ice and snow, he is the self-proclaimed king of the Ice Kingdom, a land of ice he claimed as his own where he lives in the company of many penguins
+
+Ice King's crown, the source of his powers, was created in the prehistoric past by an ice elemental named Urgence Evergreen to avert a meteor from hitting the planet. After finishing the crown, arranging it to imprint its original bearer's mind while granting the user their deepest desire, Evergreen ends up being incapacitated and is forced to entrust his apprentice Gunther to wear the crown to have him destroy the meteor in his place. But having never been taught magic by Evergreen, Gunther ended up wishing to be more like his mentor from his perspective, transformed into a clone of Evergreen and is unable to stop the Cretaceous–Paleogene extinction event. The crown survived the event and would continue its first owner's wish to turn future users into insane parodies of Evergreen.
+
+As revealed in "Holly Jolly Secrets", the crown came to Petrikov when he bought it from a dock worker in northern Scandinavia. He began to lose his mind as he began to wear the crown, deteriorating in both mind and body over the years into his current state. His condition worsened after his fiancée Betty was spirited into his future, which played a role in his subconscious need for princesses.[13] Sometime before the Mushroom War, Petrikov discovered the Enchiridion and then, 996 years before the events of the series, became a guardian to a young Marceline whom he gradually referred to as Gunter as his sanity deteriorated. Eventually, having done his best not to use the crown except as a last resort, he realized he would become a threat to Marceline and wrote a letter to apologize for no longer helping her and for whatever wrong he might do with the crown possessing him.[14] After completely losing his mind, the character ends up in the company of penguins. The most notable of them is named Gunter, revealed to be an alien forced into a penguin-like form, while Ice King calls other penguins different variations of Gunther.
+
+In the second half of the series, Balskjdhalsidhalsd@#$%^!#...`;
+
+console.log(story, 'color: #097379')
